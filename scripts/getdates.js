3c8be87cefcf95b301c68c0f60bb41d8ff099e52
KOvan-addo <kelvin.vanaddo@gmail.com>
2026-07-04 23:55:40 +0000

    more updates

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,15 @@ document.getElementById("currentyear").textContent = currentYear;
 
 document.getElementById("lastModified").textContent =
     "Last Modified: " + document.lastModified;
+const links = document.querySelectorAll(".resource-list a");
+
+links.forEach(link => {
+    link.addEventListener("mouseenter", () => {
+        link.style.transform = "translateX(5px)";
+        link.style.transition = "0.2s ease";
+    });
+
+    link.addEventListener("mouseleave", () => {
+        link.style.transform = "translateX(0)";
+    });
+});
