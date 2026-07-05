4f464ebaf9760c63cb7217da2672d36ded38f737
KOvan-addo <kelvin.vanaddo@gmail.com>
2026-07-05 01:26:06 +0000

    redid everything

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,12 @@
 // getdates.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Set current year
+    // Current Year
     const yearSpan = document.getElementById("currentyear");
     if (yearSpan) {
         yearSpan.textContent = new Date().getFullYear();
     }
 
-    // Set last modified date
+    // Last Modified
     const lastModified = document.getElementById("lastModified");
     if (lastModified) {
         lastModified.textContent = `Last Modified: ${document.lastModified}`;
